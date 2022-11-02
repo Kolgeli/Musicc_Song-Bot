2e13cde19a4321057902d9675a0e86815250efb0
Kolgeli <117229104+Kolgeli@users.noreply.github.com>
2022-11-02 12:29:54 +0400

    Update text_msg.py

--- text_msg.py
+++ text_msg.py
@@ -2,12 +2,13 @@ import pyrogram
 
 START_TEXT_MSG = '🤖 Salam 👋 user!\n\n📩 Mən sənin üçün musiqi 🎶 yükləyə bilərəm ✅. Mənə musiqi 🎶 adini aşağdaki ⤵️ formatda daxil edin:\n\n*/song*  _Musiqi adi_  \n*/song*  _Müğəni adi - Musiqi adi_\n\nBəzi musiqiləri yükləmək üçün. 🎶\n\n**■ ProTips**💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)'
 
-CONFIRMATION_TEXT_MSG = "✅ Song downloaded successfully!\n\n**■ ProTips**💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)"
+CONFIRMATION_TEXT_MSG = "✅ Digər musiqiləri yükləmək üçün!\n\n**■ Məsləhətlər**💡[ »Song Bot](https://http://t.me/Musiccsongbot) \n                          [»💻 OWNER](https://t.me/sesizKOLGE)\n                          [»Kanal](https://t.me/sesizKOLGE)\n                          [»Song Bot](https://http://t.me/Musiccsongbot)"
 
-SPOTIFY_INPUT_ERROR_TEXT_MSG = '‼️ *Oops! The bot does not support Spotify links!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n**■ ProTips**💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)'
+SPOTIFY_INPUT_ERROR_TEXT_MSG = '‼️ *Opss! The bot does not support Spotify links!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n**■ ProTips**💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)'
 
-INVALID_COMMAND_ERROR_TEXT_MSG = '‼️ *Oops! Invalid command!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n**■ ProTips**💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)'
+INVALID_COMMAND_ERROR_TEXT_MSG = '‼️ *Xəta ℹ! Yanliş Əmr!*\n"*/song* _Musiqi Adi_"\n"*/song* _Müğənni Adi - Musiqi Adi_'
 
 TOO_LONG_ERROR_TEXT_MSG = '‼️ *Oops! Video too long to convert!*\nOrder something 30 minutes or less.\n\n**■ ProTips**💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)'
 
 
+8
